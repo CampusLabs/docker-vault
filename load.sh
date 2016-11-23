694df1737854989249b9a64892b7aa73f4e523f7
Joshua Griffith <jgriffith@campuslabs.com>
2016-11-23 14:23:33 -0600

    Ensure consul is up and healthy before starting vault

--- load.sh
+++ load.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 sed -i -- "s|CONSUL_ADDRESS|$CONSUL_ADDRESS|g" $VAULT_CONFIG
+
+until $(curl --output /dev/null --silent --head --fail http://${CONSUL_ADDRESS}/v1/health/service/consul); do
+  sleep 1
+done
+
 $VAULT_ENTRYPOINT "$@" &
 
 until $(curl --output /dev/null --silent --head --fail ${VAULT_ADDR}/sys/seal-status); do
